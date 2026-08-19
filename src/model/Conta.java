00ebadd6a903f1f5cd9f97e3263e03c5e8657881
Lucas Perrotta Barbosa <perrotta.lucas@gmail.com>
2026-08-18 21:43:34 -0300

    Cria classe abstrata Conta com atributos numero, titular e saldo.

---
+++ src/model/Conta.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author lucasperrottabarbosa
+ */
+public abstract class Conta {
+    protected int numero;
+    protected String titular;
+    protected double saldo;
+}
