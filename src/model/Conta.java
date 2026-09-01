0389d90d3f42ca2c2cd1910c1b136e471eb3cf9a
Lucas Perrotta Barbosa <perrotta.lucas@gmail.com>
2026-09-01 20:54:04 -0300

    Adiciona funcionalidade de exibição de informações da Conta

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -10,13 +10,19 @@ import exception.SaldoInsuficienteException;
  * @author lucasperrottabarbosa
  */
 public abstract class Conta {
-    protected int numero;
+    protected int    numero ;
     protected String titular;
-    protected double saldo;
+    protected double saldo  ;
     
     public abstract void sacar(double Valor) throws SaldoInsuficienteException;
     
     public void depositar(double valor) {
         this.saldo += saldo;
+    }
+    
+    public void imprimirConta() {
+        IO.println("Nome: "+this.titular) ;
+        IO.println("Nº: "+this.numero)    ;
+        IO.println("Saldo: "+this.saldo)  ;
     }
 }
