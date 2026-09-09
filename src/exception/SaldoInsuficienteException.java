fc548b268d1fa13637eb272cd235e8e2b6d7280a
Lucas  Perrotta Barbosa <perrotta.lucas@gmail.com>
2026-09-09 18:23:42 -0300

    gitflow-feature-stash: ContaCorrente

--- src/exception/SaldoInsuficienteException.java
+++ src/exception/SaldoInsuficienteException.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exception;
 
-/**
- *
- * @author cg3032515
- */
 public class SaldoInsuficienteException extends Exception {
     public SaldoInsuficienteException(String mensagem) {
         super(mensagem);
